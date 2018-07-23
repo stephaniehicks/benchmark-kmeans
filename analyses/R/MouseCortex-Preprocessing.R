81af2e36851a3eeb330cac09fab2939941df7c6a
ShenzhiS <41023011+ShenzhiS@users.noreply.github.com>
2018-07-23 15:38:56 -0400

    Updating MouseCortex-Preprocessing.R

--- analyses/R/MouseCortex-Preprocessing.R
+++ analyses/R/MouseCortex-Preprocessing.R
@@ -2,7 +2,7 @@ library(SingleCellExperiment)
 library(scater)
 
 #read in data
-#sce <- readRDS("~/Downloads/2015-Zeisel-MouseCortex.RDS")
+sce <- readRDS("~/Downloads/2015-Zeisel-MouseCortex.RDS")
 sce
 
 example_sce <- calculateQCMetrics(sce)
@@ -14,7 +14,9 @@ keep_feature <- nexprs(example_sce, byrow=TRUE) >= 4
 example_sce <- example_sce[keep_feature,]
 
 #filter by cells
-keep.total <- example_sce$total_counts > 1e4
+#keep.total <- isOutlier(example_sce$total_counts, nmads=.5, 
+#                       type="lower", log=TRUE)
+keep.total <- example_sce$total_counts > 1e3
 keep.n <- example_sce$total_features_by_counts > 500
 filtered <- example_sce[,keep.total & keep.n]
 dim(filtered)
@@ -25,10 +27,14 @@ summary(sizeFactors(filtered))
 norm <- normalize(filtered)
 dim(norm)
 
+
+
 #get counts and colData from preprocessed and normalized data
-countTable <- assay(norm, "counts") # count table with gene along the rows and cells along the columns
+normlizedmousedata <- assay(norm, "logcounts") # count table with gene along the rows and cells along the columns
 mouseMeta <- colData(norm)         # phenotypic table containing meta information about dataset
 
+center <- colMeans(normlizedmousedata)
+countTable <- normlizedmousedata - rep(center, rep.int(nrow(normlizedmousedata), ncol(normlizedmousedata)))
+
 #save to .csv file
-write.csv(countTable, file = "mouseCounts.csv")
-write.csv(mouseMeta, file = "mouseMeta.csv")
+write.csv(countTable, file = "~/Desktop/2018summerintern/python/mouseCounts.csv")
