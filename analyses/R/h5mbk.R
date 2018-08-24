9c29b6f6f949b8dffd85fc0a46da333d5b7d4158
ShenzhiS <41023011+ShenzhiS@users.noreply.github.com>
2018-08-24 12:55:41 -0400

    Adding R script for minibatchkmeans on hdf5 file function

--- analyses/R/h5mbk.R
+++ analyses/R/h5mbk.R
@@ -1,17 +1,23 @@
 #' Performs mini batch kmeans clustering on an hdf5 file
-#' 
+#' @author Shenzhi Shi
+#' @import BiocSklearn
 #' @param file name of hdf5 file.
 #' @param name name of object in hdf5 file.
 #' @param mbk MiniBatchKMeans object. Use to specify n_clusters, etc.
+#'
 #' @return An array of integers indicating the cluster of each point
 #' @examples
-#' testmbk <- h5mbk("py-mouse-data.h5", "mousedata", SklearnEls()$skcl$MiniBatchKMeans(n_clusters = 3L))
+#' mbk <- SklearnEls()$skcl$MiniBatchKMeans(n_clusters = 3L)
+#' testmbk <- h5mbk(file = "py-mouse-data.h5", name = "mousedata", mbk = mbk)
 h5mbk <- function(file = "", name = "", mbk = SklearnEls()$skcl$MiniBatchKMeans(), prediction = array(), first = TRUE, i = 1){
+  #get data
   f <- SklearnEls()$h5py$File(file, "r")
   data <- f$get(name)
+  #if it's the first time running, set return value to the same length as the number of samples
   if(first){
     prediction <- array(dim = data$shape[2])
   }
+  #check for the end of the samples
   if ((i+2) > data$shape[2]){
     if(i <= data$shape[2]){
       mbk$partial_fit(t(data$value[,i:data$shape[2]]))
@@ -24,10 +30,12 @@ h5mbk <- function(file = "", name = "", mbk = SklearnEls()$skcl$MiniBatchKMeans(
       return(prediction)
     }
   }
+  #read in a slice of 3 samples and update prediction with minibatchkmeans partial_fit
   else {
     mbk$partial_fit(t(data$value[,i:(i+2)]))
     prediction[i:(i+2)] <- mbk$predict(t(data$value[,i:(i+2)]))
     f$close()
+    #continue loop
     return(h5mbk(file, name, mbk, prediction, first = FALSE,(i+3)))
   }
 }
